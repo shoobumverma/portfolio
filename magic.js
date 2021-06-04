2aa44a98093fd0e7fec5f8f22dd3b57dbdf66902
shoobum <55514905+shoobumverma@users.noreply.github.com>
2021-06-04 20:19:32 +0200

    fixes to performance

--- magic.js
+++ magic.js
@@ -1,9 +1,19 @@
+//Scroll to top button
+var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop,
-    topBtn = document.getElementById("topBtn");
-  if (scrolled > 300) {
+  var currentScrollPos = window.pageYOffset;
+  console.log(currentScrollPos);
+  topBtn = document.getElementById("topBtn");
+  if (currentScrollPos < 30) {
+    topBtn.classList.remove("visible");
+  } else if (prevScrollpos > currentScrollPos) {
     topBtn.classList.add("visible");
   } else {
     topBtn.classList.remove("visible");
   }
+  prevScrollpos = currentScrollPos;
 };
+
+function scrollToTop() {
+  window.scroll({ top: 0, behavior: "smooth" });
+}
